dc5434d5d762302dd6073f0ac6597478d353a20d
freefireman <beluh.c.s@gmail.com>
2020-11-25 21:03:50 +0200

    little fix: adding check for registration in documentHtml.js

--- bot_service/on/documentHtml.js
+++ bot_service/on/documentHtml.js
@@ -6,32 +6,36 @@ const {
     MIMETYPE: mimeType
 } = require('./../../constants')
 
-module.exports =  (bot) => {
+module.exports = (bot) => {
 
-    bot.on('document',async ctx => {
+    bot.on('document', async ctx => {
 
-        const idChat = fs.readFileSync(path.join(process.cwd(),'chat.txt'), "utf8")
+        const idChat = fs.readFileSync(path.join(process.cwd(), 'chat.txt'), "utf8")
         const {mime_type} = ctx.message.document
-        const userObj = {...ctx.from,...ctx.message.document}
+        const userObj = {...ctx.from, ...ctx.message.document}
         // console.log('mime_type');
         // console.log(mime_type);
-        if(mime_type === mimeType.TEXT_HTML
-            || mime_type === mimeType.TEXT_CSS
-            || mime_type === mimeType.APPLICATION_RAR
-            || mime_type === mimeType.APPLICATION_ZIP
-            || mime_type === mimeType.APPLICATION_RAR_VND
-        ) {
+        if(await userService.isRegUser(ctx.from.id)){
+            if (mime_type === mimeType.TEXT_HTML
+                || mime_type === mimeType.TEXT_CSS
+                || mime_type === mimeType.APPLICATION_RAR
+                || mime_type === mimeType.APPLICATION_ZIP
+                || mime_type === mimeType.APPLICATION_RAR_VND
+            ) {
 
-            userService.logUserFileSending(userObj)
-            ctx.forwardMessage(idChat,ctx.message.id).then(res=>{
-                ctx.reply('file is forwarding')
-            }).catch(err=>{
-                console.log(err);
-                ctx.reply('file is NOT forwarding')
-            })
+                userService.logUserFileSending(userObj)
+                ctx.forwardMessage(idChat, ctx.message.id).then(res => {
+                    ctx.reply('file is forwarding')
+                }).catch(err => {
+                    console.log(err);
+                    ctx.reply('file is NOT forwarding')
+                })
+            } else {
+                console.log(ctx.message);
+                ctx.reply('file is not accessing')
+            }
         }else{
-            console.log(ctx.message);
-            ctx.reply('file is not accessing')
+            ctx.reply('Ви повинні доти свій контакт, кнопка знизу')
         }
 
     });
