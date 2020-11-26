391240b8373f931f7ee0439d07e09b1ab03b055e
freefireman <beluh.c.s@gmail.com>
2020-11-26 20:47:40 +0200

    adding privat char in forward documentHtml.js

--- bot_service/on/documentHtml.js
+++ bot_service/on/documentHtml.js
@@ -10,33 +10,35 @@ module.exports = (bot) => {
 
     bot.on('document', async ctx => {
 
-        const idChat = fs.readFileSync(path.join(process.cwd(), 'chat.txt'), "utf8")
-        const {mime_type} = ctx.message.document
-        const userObj = {...ctx.from, ...ctx.message.document}
-        // console.log('mime_type');
-        // console.log(mime_type);
-        if(await userService.isRegUser(ctx.from.id)){
-            if (mime_type === mimeType.TEXT_HTML
-                || mime_type === mimeType.TEXT_CSS
-                || mime_type === mimeType.APPLICATION_RAR
-                || mime_type === mimeType.APPLICATION_ZIP
-                || mime_type === mimeType.APPLICATION_RAR_VND
-            ) {
+       if(ctx.update.message.chat.type === 'private'){
+           const idChat = fs.readFileSync(path.join(process.cwd(), 'chat.txt'), "utf8")
+           const {mime_type} = ctx.message.document
+           const userObj = {...ctx.from, ...ctx.message.document}
+           // console.log('mime_type');
+           // console.log(mime_type);
+           if(await userService.isRegUser(ctx.from.id)){
+               if (mime_type === mimeType.TEXT_HTML
+                   || mime_type === mimeType.TEXT_CSS
+                   || mime_type === mimeType.APPLICATION_RAR
+                   || mime_type === mimeType.APPLICATION_ZIP
+                   || mime_type === mimeType.APPLICATION_RAR_VND
+               ) {
 
-                userService.logUserFileSending(userObj)
-                ctx.forwardMessage(idChat, ctx.message.id).then(res => {
-                    ctx.reply('file is forwarding')
-                }).catch(err => {
-                    console.log(err);
-                    ctx.reply('file is NOT forwarding')
-                })
-            } else {
-                console.log(ctx.message);
-                ctx.reply('file is not accessing')
-            }
-        }else{
-            ctx.reply('Ви повинні дати свій контакт, кнопка знизу')
-        }
+                   userService.logUserFileSending(userObj)
+                   ctx.forwardMessage(idChat, ctx.message.id).then(res => {
+                       ctx.reply('file is forwarding')
+                   }).catch(err => {
+                       console.log(err);
+                       ctx.reply('file is NOT forwarding')
+                   })
+               } else {
+                   console.log(ctx.message);
+                   ctx.reply('file is not accessing')
+               }
+           }else {
+               ctx.reply('Ви повинні дати свій контакт, кнопка знизу')
+           }
+       }
 
     });
 }
